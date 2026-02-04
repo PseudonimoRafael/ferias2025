a4aec99ffd01ce0d77451535f98a02fb0e63d220
Maximus <rafinhamaximus3141592@gmail.com>
2026-02-04 01:30:53 -0300

    má implemnetação heapfy
    Signed-off-by: Maximus <rafinhamaximus3141592@gmail.com>

--- sorters/heapSort/heapfy.go
+++ sorters/heapSort/heapfy.go
@@ -2,18 +2,17 @@ package heapSort
 
 func heapify(array []int, n, i int) {
 	// n -> tamanho do array completo; i -> topo do heap
-	maior := array[i]
 	
 	m := i
 	l := i << 1
 	l += 1
 	r := l + 1 // l := i * 2 + 1; r := i * 2 + 2
 
-	if l < n && array[l] > maior {
+	if l < n && array[l] > array[m] {
 		m = l
 	}
 
-	if r < n && array[r] > maior {
+	if r < n && array[r] > array[m] {
 		m = r
 	}
 
